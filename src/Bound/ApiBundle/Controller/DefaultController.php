edc42470c8e1edab80c487a14e2dfa539fd7d50a
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-10-13 16:59:03 +0200

    First implementation of ApiBundle + Services

--- src/Bound/ApiBundle/Controller/DefaultController.php
+++ src/Bound/ApiBundle/Controller/DefaultController.php
@@ -4,10 +4,20 @@ namespace Bound\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller {
+
+    public function indexAction($name) {
         return $this->render('BoundApiBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function showAction($title, $content, $author) {
+        $array = array('response' => "200", 'data' => array('title' => $title, 'content' => $content, 'author' => $author));
+        $response = new Response(json_encode($array, JSON_PRETTY_PRINT));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response; 
     }
 }
--- src/Bound/CoreBundle/Resources/public/js/controllers/mainController.js
+++ src/Bound/CoreBundle/Resources/public/js/controllers/mainController.js
@@ -2,10 +2,19 @@
 * @Author: gicque_p
 * @Date:   2015-10-12 13:32:03
 * @Last Modified by:   gicque_p
-* @Last Modified time: 2015-10-12 13:32:49
+* @Last Modified time: 2015-10-13 16:57:22
 */
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', 'showNews', function($scope) {
+
+    showNews.get()
+    .success(function(data) {
+        $scope.news = data;
+    })
+    .error(function(err) {
+        $scope.status = 'Unable to load news data: ' + err.message;
+    });
+
     $scope.apps = [
         {
           icon: 'img/move.jpg',
