11c611c5eab2e5c5f9833c8f507f5669e25b0868
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-10-15 16:48:35 +0200

    Add Services layout on Manager

--- src/Bound/ApiBundle/Controller/UserController.php
+++ src/Bound/ApiBundle/Controller/UserController.php
@@ -6,27 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class UserController extends Controller {
 
     public function getAction(Request $request) {
-        $resp = array();
         $entities = $this->getDoctrine()->getRepository('BoundUserBundle:User')->findAll();
 
+        $users = array();
         if (!empty($entities)) {        
-            $users = array();
             foreach ($entities as $entity) {
                 $users[$entity->getSalt()] = $entity->toArray();
             }
 
-            $resp['response'] = 200;
-            $resp['users'] = $users;
+            $status = 200;
         } else {
-            $resp['response'] = 500;
+            $status = 500;
         }
 
-        $response = new Response(json_encode($resp, JSON_PRETTY_PRINT));
-        $response->headers->set('Content-Type', "application/json");
+        $response = new JsonResponse($users, $status);
+        $response->setEncodingOptions(JSON_PRETTY_PRINT);
 
         return $response;
     }
---
+++ src/Bound/CoreBundle/Manager/UserManager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @Author: gicque_p
+ * @Date:   2015-10-15 16:31:53
+ * @Last Modified by:   gicque_p
+ * @Last Modified time: 2015-10-15 16:48:12
+ */
+
+namespace Bound\CoreBundle\Manager;
+
+use Symfony\Component\DependencyInjection\Container;
+
+class UserManager {
+
+    public function modify() {
+        
+    }
+};
