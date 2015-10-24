e212e4f0232fb07cb6a70bba56d38d5c8e90946e
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-10-24 12:48:26 +0200

    Add animation for content + add font awesome

--- src/Bound/FrontBundle/Controller/DefaultController.php
+++ src/Bound/FrontBundle/Controller/DefaultController.php
@@ -4,9 +4,19 @@ namespace Bound\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 class DefaultController extends Controller {
 
     public function indexAction() {
-        return $this->render('BoundFrontBundle::layout.html.twig');
+        $securityContext = $this->container->get('security.context');
+
+        if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY') or $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->render('BoundFrontBundle::layout.html.twig');
+        } else {
+            return new JsonResponse(array());
+        }
     }
 }
