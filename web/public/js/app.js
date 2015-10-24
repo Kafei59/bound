6453fd46d47f5c6a572d1822452ba918ea94ab34
Kafei59 <pierrick.gicquelais@epitech.eu>
2015-10-24 18:06:25 +0200

    Fix issues

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -2,7 +2,7 @@
 * @Author: gicque_p
 * @Date:   2015-10-12 13:24:59
 * @Last Modified by:   gicque_p
-* @Last Modified time: 2015-10-24 15:51:27
+* @Last Modified time: 2015-10-24 18:02:34
 */
 
 var path = '../public/views/';
