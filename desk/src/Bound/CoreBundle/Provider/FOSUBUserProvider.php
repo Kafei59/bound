9d598e3eb32aa849ee45d764ab19b7735cfbe5a3
Kafei59 <pierrick.gicquelais@epitech.eu>
2016-02-16 11:12:22 +0100

    Add Provider for Register/Login Facebook

--- desk/src/Bound/CoreBundle/Provider/FOSUBUserProvider.php
+++ desk/src/Bound/CoreBundle/Provider/FOSUBUserProvider.php
@@ -10,6 +10,9 @@ use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+use Bound\CoreBundle\Entity\User;
+use Bound\CoreBundle\Entity\Client;
+
 class FOSUBUserProvider extends BaseClass {
 
     private $container;
@@ -22,41 +25,75 @@ class FOSUBUserProvider extends BaseClass {
         $this->manager = $manager;
     }
 
-    public function connect(UserInterface $user, UserResponseInterface $response) {
-        $property = $this->getProperty($response);
-        $username = $response->getUsername();
+    // public function connect(UserInterface $user, UserResponseInterface $response) {
+    //     $property = $this->getProperty($response);
+    //     $username = $response->getUsername();
+
+    //     $service = $response->getResourceOwner()->getName();
+    //     $setter = 'set'.ucfirst($service);
+    //     $setter_id = $setter.'Id';
+    //     $setter_token = $setter.'AccessToken';
+
+    //     $previousUser = $this->userManager->findUserByUsername($username);
+
+    //     if ($previousUser != NULL) {
+    //         $client = $previousUser->getClient();
+    //         $client->$setter_id($username);
+    //         $client->$setter_token($response->getAccessToken());
+
+    //         $this->manager->persist($client);
+    //         $this->manager->flush();
+    //     }
+    // }
+
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response) {
+        $email = $response->getEmail();
+
+        $user = $this->userManager->findUserByEmail($email);
+        if ($user instanceof User) {
+            return $this->login($response, $user);
+        } else {
+            return $this->register($response);
+        }
+    }
+
+    private function register(UserResponseInterface $response) {
+        $username = $response->getRealName();
+        $email = $response->getEmail();
+        $password = $response->getUsername();
 
         $service = $response->getResourceOwner()->getName();
         $setter = 'set'.ucfirst($service);
         $setter_id = $setter.'Id';
         $setter_token = $setter.'AccessToken';
 
-        $previousUser = $this->userManager->findUserByUsername($username);
+        $client = $this->container->get('doctrine')->getRepository('BoundCoreBundle:Client')->findOneBy(array('facebook_id' => $password));
+        if ($client instanceof Client) {
+            throw new Exception(409, "Account already exists");
+        } else {
+            $user = $this->container->get('bound.user_manager')->add($username, $email, $password);
+            $client = $user->getClient();
 
-        if ($previousUser != NULL) {
-            $client = $previousUser->getClient();
-            $client->$setter_id($username);
+            $client->$setter_id($password);
             $client->$setter_token($response->getAccessToken());
 
             $this->manager->persist($client);
             $this->manager->flush();
+
+            return $user;
         }
     }
 
-    public function loadUserByOAuthUserResponse(UserResponseInterface $response) {
-        $username = $response->getRealName();
-        $email = $response->getEmail();
-        $password = $response->getUsername();
+    private function login(UserResponseInterface $response, User $user) {
+        $client = $user->getClient();
+        if ($client instanceof Client) {
+            $property = $this->getProperty($response);
+            $username = $response->getUsername();
 
-        $user = $this->userManager->findUserByEmail($email);
-        if ($user == NULL) {
             $service = $response->getResourceOwner()->getName();
             $setter = 'set'.ucfirst($service);
             $setter_id = $setter.'Id';
             $setter_token = $setter.'AccessToken';
-
-            $user = $this->container->get('bound.user_manager')->add($username, $email, $password);
-            $client = $user->getClient();
 
             $client->$setter_id($username);
             $client->$setter_token($response->getAccessToken());
@@ -66,14 +103,7 @@ class FOSUBUserProvider extends BaseClass {
 
             return $user;
         } else {
-            // $user = parent::loadUserByOAuthUserResponse($response);
-            $serviceName = $response->getResourceOwner()->getName();
-            $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
-
-            $client = $user->getClient();
-            $client->$setter($response->getAccessToken());
-
-            return $user;
+            throw new Exception(404, "Client not found");
         }
     }
 }
