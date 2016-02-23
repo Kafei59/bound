69107f5e5069b45cea8544414aa675d1b434c116
Kafei59 <pierrick.gicquelais@epitech.eu>
2016-02-23 14:47:45 +0100

    Add Globals Type

---
+++ desk/src/Bound/CoreBundle/Globals/AchievementType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Author: gicque_p
+ * @Date:   2016-02-23 14:44:50
+ * @Last Modified by:   gicque_p
+ * @Last Modified time: 2016-02-23 14:45:53
+ */
+
+namespace Bound\CoreBundle\Globals;
+
+final class AchievementType {
+    const BOUND = "bound";
+    const FACEBOOK = "facebook";
+    const TWITTER = "twitter";
+}
---
+++ desk/src/Bound/CoreBundle/Globals/NotificationType.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Author: gicque_p
+ * @Date:   2016-02-23 14:45:03
+ * @Last Modified by:   gicque_p
+ * @Last Modified time: 2016-02-23 14:46:18
+ */
+
+namespace Bound\CoreBundle\Globals;
+
+final class NotificationType {
+    const ACHIEVEMENT = "achievement";
+    const FRIEND = "friend";
+    const FACEBOOK = "facebook";
+    const TWITTER = "twitter";
+}
